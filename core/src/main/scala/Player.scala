0a5c76647a618f6e58ab5724ab4e1584f9d91ede
Carmine Vattimo <carmi230192@hotmail.com>
2017-07-18 23:41:43 +0200

    Refactor of Poi and Player Traits. Bug fixes in PositionableTest.

--- core/src/main/scala/Player.scala
+++ core/src/main/scala/Player.scala
@@ -11,3 +11,27 @@ trait Player extends Positionable{
   def setPosition (newPosition: Position)
 
 }
+
+/** A player of the treasure hunt
+  *
+  */
+class PlayerImpl extends Player{
+
+  var position : Option[Position] = Option.empty[Position]
+
+  /** Sets a new position of the player
+    *
+    *  @param newPosition the new player position
+    */
+  override def setPosition (newPosition: Position) = {
+    position = Some(newPosition)
+  }
+
+  /** Returns the position of the player
+    *
+    *  @return an option of position
+    */
+  override def getPosition: Option[Position] = {
+    position
+  }
+}
--- core/src/main/scala/Poi.scala
+++ core/src/main/scala/Poi.scala
@@ -5,3 +5,17 @@
 trait Poi extends Positionable{
 
 }
+
+/** A point of interest on the map
+  *
+  */
+class PoiImpl(position: Position) extends Positionable{
+
+    /** Returns the position of the point of interest
+      *
+      *  @return an option of position
+      */
+    override def getPosition: Option[Position] = {
+        Some(position)
+    }
+}
