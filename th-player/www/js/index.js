ed8978fc340a5673a20a7867f5ecc81e0df6a549
Carmine Vattimo <carmi230192@hotmail.com>
2017-08-07 18:47:50 +0200

    Reformat of PR #20

--- th-player/www/js/index.js
+++ th-player/www/js/index.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 //--------------------------GLOBAL-VARIABLES--------------------------------
-var lastClueText="text text text text text text text text text text text text text text text text text text ";
+var lastClueText = "text text text text text text text text text text text text text text text text text text ";
 var loggedTeam;
 var save;
 var codeSave;
@@ -25,10 +25,10 @@ var app = {
     // Application Constructor
     initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-        loggedTeam=localStorage.getItem('loggedTeam');
-        if (loggedTeam!=null){
+        loggedTeam = localStorage.getItem('loggedTeam');
+        if (loggedTeam != null) {
             showLoggedTeamName();
-        }else{
+        } else {
             showNotLoggedUser();
         }
         if (codeSave != null) {
@@ -78,7 +78,7 @@ var app = {
                 function (error) {
                     alert("Scanning failed: " + error);
                 })
-            document.getElementById("second").style.display="block";
+            document.getElementById("second").style.display = "block";
             document.getElementById("first").style.display = "none";
             loadSecondPage();
 
@@ -118,16 +118,18 @@ var app = {
     }
 
 };
+
 //-------------------------------MAP----------------------------------------
-function loadSecondPage(){
+function loadSecondPage() {
     var mapScript = document.createElement('script');
-    mapScript.setAttribute('src','https://maps.googleapis.com/maps/api/js?key=AIzaSyBNQr4YcrvttSMIgWOX68kJnigaI0Cir9c&callback=mapLoadedCallback');
+    mapScript.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBNQr4YcrvttSMIgWOX68kJnigaI0Cir9c&callback=mapLoadedCallback');
     document.head.appendChild(mapScript);
 }
+
 function mapLoadedCallback() {
-    getLocation(function(firstLatitude,firstLongitude){
+    getLocation(function (firstLatitude, firstLongitude) {
         var mapProp = {
-            center: new google.maps.LatLng(firstLatitude,firstLongitude),
+            center: new google.maps.LatLng(firstLatitude, firstLongitude),
             zoom: 15,
         };
         var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
@@ -136,120 +138,140 @@ function mapLoadedCallback() {
             map: map,
             title: "player-position"
         });
-        setInterval(function(){
-            getLocation(function(latitude,longitude){
-                map.setCenter(new google.maps.LatLng( latitude, longitude ) );
+        setInterval(function () {
+            getLocation(function (latitude, longitude) {
+                map.setCenter(new google.maps.LatLng(latitude, longitude));
                 marker.setPosition({lat: latitude, lng: longitude});
             })
         }, 3000);
     })
 
 }
-function getLocation(callback){
-    var onSuccess = function(position) {
-        callback(position.coords.latitude,position.coords.longitude)
+
+function getLocation(callback) {
+    var onSuccess = function (position) {
+        callback(position.coords.latitude, position.coords.longitude)
     };
+
     function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
+        alert('code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n');
     }
+
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }
+
 //-------------------------------CLUE----------------------------------------
-function showLastClue(){
+function showLastClue() {
     showClue(lastClueText);
 }
-function showNewClue(text){
+
+function showNewClue(text) {
     showClue(text);
     setLastClueText(text);
 }
-function setLastClueText(text){
-    var lastClueText=text;
+
+function setLastClueText(text) {
+    var lastClueText = text;
 }
-function showClue(clueText){
+
+function showClue(clueText) {
     var container = document.getElementById("map-page-clue");
     var content = '<h1>CLUE</h1>';
-    content += '<p>'+ clueText +'</p>';
+    content += '<p>' + clueText + '</p>';
     content += '<div onClick="closeClue()" class="clue-quiz-button"><span>HIDE</span></div>';
-    container.innerHTML=content;
-    container.style.display="block";
+    container.innerHTML = content;
+    container.style.display = "block";
 }
-function closeClue(){
-    document.getElementById("map-page-clue").style.display="none";
+
+function closeClue() {
+    document.getElementById("map-page-clue").style.display = "none";
 }
 
 //---------------------------------QUIZ------------------------------------
 
-function showQuiz(question, answer){
+function showQuiz(question, answer) {
     var container = document.getElementById("map-page-quiz");
     var content = '<h1>QUIZ</h1>';
-    content += '<p>'+question+'</p>';
-    content+= '<input class="quizInput" id="quizInput" type="text" />'
-    content += '<div onClick="checkQuiz(\''+answer+'\')" class="clue-quiz-button"><span>CHECK</span></div>';
-    container.innerHTML=content;
-    container.style.display="block";
+    content += '<p>' + question + '</p>';
+    content += '<input class="quizInput" id="quizInput" type="text" />'
+    content += '<div onClick="checkQuiz(\'' + answer + '\')" class="clue-quiz-button"><span>CHECK</span></div>';
+    container.innerHTML = content;
+    container.style.display = "block";
 }
-function checkQuiz(answer){
-    if(document.getElementById("quizInput").value.trim()==answer){
-        document.getElementById("map-page-quiz").style.display="none";
+
+function checkQuiz(answer) {
+    if (document.getElementById("quizInput").value.trim() == answer) {
+        document.getElementById("map-page-quiz").style.display = "none";
     }
 }
+
 //---------------------------------PROGRESS---------------------------------
-function viewProgress(){
-    getProgress(function(totalPOI,reachedPOI){
+function viewProgress() {
+    getProgress(function (totalPOI, reachedPOI) {
         var div = document.getElementById("map-page-progress");
-        var content="";
-        var value=totalPOI/reachedPOI*10+20;
-        content += '<div class="closeProgress" onclick="closeProgress()">X</div><div class="progressPercent">'+Math.round(value)+'%</div><div class="outOfProgress">'+reachedPOI+'</br>POI</br>reached</br>out of</br>'+totalPOI+'</div><div class="progressBar"><div class="progressBarContent animated bounceInDown" style="height:'+value+'%;"></div></div>';
-        div.innerHTML=content;
-        div.style.display="block";
+        var content = "";
+        var value = totalPOI / reachedPOI * 10 + 20;
+        content += '<div class="closeProgress" onclick="closeProgress()">X</div><div class="progressPercent">' + Math.round(value) + '%</div><div class="outOfProgress">' + reachedPOI + '</br>POI</br>reached</br>out of</br>' + totalPOI + '</div><div class="progressBar"><div class="progressBarContent animated bounceInDown" style="height:' + value + '%;"></div></div>';
+        div.innerHTML = content;
+        div.style.display = "block";
     });
 }
-function getProgress(callback){
-    var totalPOI=10;
-    var reachedPOI=3;
-    callback(totalPOI,reachedPOI);
+
+function getProgress(callback) {
+    var totalPOI = 10;
+    var reachedPOI = 3;
+    callback(totalPOI, reachedPOI);
 }
-function closeProgress(){
-    document.getElementById("map-page-progress").style.display="none";
+
+function closeProgress() {
+    document.getElementById("map-page-progress").style.display = "none";
 }
+
 //---------------------------------LOGIN---------------------------------
-function showLoggedTeamName(){
-    userInfo=document.getElementById("userInfo");
-    userInfo.innerHTML='<span>Team: '+loggedTeam+'</span>';
-    userInfo.style.display="block";
+function showLoggedTeamName() {
+    userInfo = document.getElementById("userInfo");
+    userInfo.innerHTML = '<span>Team: ' + loggedTeam + '</span>';
+    userInfo.style.display = "block";
 }
-function showNotLoggedUser(){
-    document.getElementById("notLoggedUser").style.display="block";
-    document.getElementById("createOrLogin").style.display="block";
-    document.getElementById("loginTeam").style.display="none";
-    document.getElementById("createTeam").style.display="none";
+
+function showNotLoggedUser() {
+    document.getElementById("notLoggedUser").style.display = "block";
+    document.getElementById("createOrLogin").style.display = "block";
+    document.getElementById("loginTeam").style.display = "none";
+    document.getElementById("createTeam").style.display = "none";
 }
-function showCreateTeamPage(){
-    document.getElementById("createOrLogin").style.display="none";
-    document.getElementById("createTeam").style.display="block";
+
+function showCreateTeamPage() {
+    document.getElementById("createOrLogin").style.display = "none";
+    document.getElementById("createTeam").style.display = "block";
     document.getElementById("createTeamNameInput").focus();
 }
-function createTeam(){
-    document.getElementById("notLoggedUser").style.display="none";
+
+function createTeam() {
+    document.getElementById("notLoggedUser").style.display = "none";
     loggedTeam = document.getElementById("createTeamNameInput").value;
-    if(document.getElementById("createTeamPass1Input").value==document.getElementById("createTeamPass2Input").value){
-    //TODO create team
+    if (document.getElementById("createTeamPass1Input").value == document.getElementById("createTeamPass2Input").value) {
+        //TODO create team
     }
     showLoggedTeamName()
 }
-function showLoginTeamPage(){
-    document.getElementById("createOrLogin").style.display="none";
-    document.getElementById("loginTeam").style.display="block";
+
+function showLoginTeamPage() {
+    document.getElementById("createOrLogin").style.display = "none";
+    document.getElementById("loginTeam").style.display = "block";
     document.getElementById("loginTeamNameInput").focus();
 }
-function loginTeam(){
-    document.getElementById("notLoggedUser").style.display="none";
+
+function loginTeam() {
+    document.getElementById("notLoggedUser").style.display = "none";
     //TODO check correct data
     loggedTeam = document.getElementById("loginTeamNameInput").value;
     showLoggedTeamName()
 }
-function logoutTeam(){
-    document.getElementById("createOrLogin").style.display="block";
+
+function logoutTeam() {
+    document.getElementById("createOrLogin").style.display = "block";
 }
+
 app.initialize();
