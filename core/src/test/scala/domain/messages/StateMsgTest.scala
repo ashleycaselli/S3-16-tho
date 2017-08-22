5b0d350d62d6572cef5f561cc5e089e994323ce6
Ashley Caselli <ashley.caselli@studio.unibo.it>
2017-08-22 18:02:28 +0200

    StateMsg refactored and updated. CreatedStateMsg added to the State enumeration. StateMsg tests refactored, CreatedStateMsg test added.

--- core/src/test/scala/domain/messages/StateMsgTest.scala
+++ core/src/test/scala/domain/messages/StateMsgTest.scala
@@ -1,6 +1,7 @@
 package domain.messages
 
 import com.typesafe.scalalogging.Logger
+import domain.messages.State.State
 import org.scalatest.{BeforeAndAfter, FunSuite}
 import org.slf4j.LoggerFactory
 import play.api.libs.json.Json
@@ -8,15 +9,16 @@ import play.api.libs.json.Json
 class StateMsgTest extends FunSuite with BeforeAndAfter {
 
     private val sender = "Organizer"
-    private var start: String = State.Start.toString
-    private var stop: String = State.Stop.toString
+    private val start: String = State.Start.toString
+    private val stop: String = State.Stop.toString
     private var startMessage: StartMsgImpl = null
     private var stopMessage: StopMsgImpl = null
     val logger = Logger(LoggerFactory.getLogger("test"))
+    private val th = "TH-0"
 
     before {
-        startMessage = StartMsgImpl(sender, start)
-        stopMessage = StopMsgImpl(sender, stop)
+        startMessage = StartMsgImpl(sender, treasureHuntID = th)
+        stopMessage = StopMsgImpl(sender, treasureHuntID = th)
     }
 
     test("StartMsg sender and payload must be String") {
@@ -36,19 +38,21 @@ class StateMsgTest extends FunSuite with BeforeAndAfter {
     }
 
     test("Checking StartMsg's serializability") {
-        implicit val startMsgReads = Json.reads[StartMsgImpl]
-        val newStartMsg = Json.parse(startMessage.defaultRepresentation).as[StartMsgImpl]
-        logger.info(s"oldStartMsg: ${startMessage.defaultRepresentation.toString}")
-        logger.info(s"newStartMsg: ${newStartMsg.defaultRepresentation.toString}")
-        assert(startMessage === newStartMsg)
+        val rep = startMessage.defaultRepresentation
+        logger.info(rep)
+        val json = Json.parse(rep)
+        assert((json \ "sender").as[String] === sender)
+        assert(((json \ "payload") \ "th").as[String] === th)
+        assert(((json \ "payload") \ "state").as[State] === State.Start)
     }
 
     test("Checking StopMsg's serializability") {
-        implicit val stopMsgReads = Json.reads[StopMsgImpl]
-        val newStopMsg = Json.parse(stopMessage.defaultRepresentation).as[StopMsgImpl]
-        logger.info(s"oldStopMsg: ${stopMessage.defaultRepresentation.toString}")
-        logger.info(s"newStopMsg: ${newStopMsg.defaultRepresentation.toString}")
-        assert(stopMessage === newStopMsg)
+        val rep = stopMessage.defaultRepresentation
+        logger.info(rep)
+        val json = Json.parse(rep)
+        assert((json \ "sender").as[String] === sender)
+        assert(((json \ "payload") \ "th").as[String] === th)
+        assert(((json \ "payload") \ "state").as[State] === State.Stop)
     }
 
 }
