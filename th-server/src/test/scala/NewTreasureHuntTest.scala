b4d0ce86d9dfe54fbcae04cd5e50642e06330d44
Carmine Vattimo <carmi230192@hotmail.com>
2017-08-24 20:17:26 +0200

    New Treasure Hunt in DB Created. Treasure Hunt insert tested succesfully.

---
+++ th-server/src/test/scala/NewTreasureHuntTest.scala
@@ -0,0 +1,50 @@
+import java.sql.Connection
+
+import org.joda.time.DateTime
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import utils.{DBConnectionManager, DBConnectionManagerImpl}
+
+class NewTreasureHuntTest extends FunSuite with BeforeAndAfter {
+    private var newTreasureHunt: NewTreasureHunt = null
+    private var connectionManager: DBConnectionManager = null
+
+    before {
+        newTreasureHunt = new NewTreasureHuntImpl
+        connectionManager = new DBConnectionManagerImpl
+    }
+
+    test("After the new treasure hunt insert, it is present in treasure_hunt table and has the correct value") {
+        val connection: Connection = connectionManager.establishConnection
+        var statement = connection.createStatement
+
+        val name: String = "TestInsertingTH"
+        val location: String = "Gotham City"
+        val startDate: DateTime = DateTime.now()
+        val startTime: String = "20:30"
+        val idOrganizer: Int = 1
+
+        new NewTreasureHuntImpl().insertNewTreasureHunt(name, location, startDate, startTime, idOrganizer)
+
+        var rs = statement.executeQuery("SELECT MAX(id_treasure_hunt) FROM treasure_hunt")
+        var idTH = 0
+        while (rs.next) {
+            idTH = rs.getInt("MAX(id_treasure_hunt)")
+        }
+
+        /*---CHECK IF INSERT IS CORRECT---*/
+        rs = statement.executeQuery(s"SELECT * FROM treasure_hunt WHERE id_treasure_hunt = ${idTH}")
+        while (rs.next) {
+            assert(rs.getString("name") == name)
+            assert(rs.getString("location") == location)
+            assert(rs.getDate("start_date").toString == startDate.toLocalDate.toString)
+            assert(rs.getString("start_time") == startTime)
+            assert(rs.getInt("id_organizer") == idOrganizer)
+        }
+
+        /*---DELETE THE LAST ADDED ROW---*/
+        statement = connection.createStatement
+        statement.executeUpdate(s"DELETE FROM treasure_hunt WHERE id_treasure_hunt = ${idTH}")
+
+        connection.close()
+    }
+}
