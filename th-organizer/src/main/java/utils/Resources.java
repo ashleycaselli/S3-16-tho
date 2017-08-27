aa4d51977b0169c265eeffb5714c419ec1f7237d
Ashley Caselli <ashley.caselli@studio.unibo.it>
2017-08-27 13:05:56 +0200

    th-organizer utils package created. Resources and Strings classes added.

---
+++ th-organizer/src/main/java/utils/Resources.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Class containing resources information
+ */
+public class Resources {
+
+    private static final String JAVAFX_VIEW_EXTENSION = ".fxml";
+
+}
---
+++ th-organizer/src/main/java/utils/Strings.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Class containing all the system's Strings
+ */
+public class Strings {
+
+    public final static String ORGANIZER_TITLE = "Treasure Hunt Organizer";
+
+}
