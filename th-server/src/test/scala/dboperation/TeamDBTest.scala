3bb41316098ba1ea5ed93f1bed54c4810f9078aa
Carmine Vattimo <carmi230192@hotmail.com>
2017-08-28 14:33:27 +0200

    Added the following features: - Create new team - Check if the chosen team name is available - Login into the mobile app
    Added tests for al the features above.

--- th-server/src/test/scala/dboperation/TeamDBTest.scala
+++ th-server/src/test/scala/dboperation/TeamDBTest.scala
@@ -27,7 +27,7 @@ class TeamDBTest extends FunSuite with BeforeAndAfter {
         var rs = statement.executeQuery(s"SELECT COUNT(*) FROM team_in_treasure_hunt WHERE id_treasure_hunt = ${idTH} AND id_team = ${idTeam}")
         var subscribtion = 0
         while (rs.next) {
-            subscribtion = rs.getInt("COUNT(*)")
+            subscribtion = rs.getInt(1)
         }
 
         assert(subscribtion == 1)
@@ -37,5 +37,54 @@ class TeamDBTest extends FunSuite with BeforeAndAfter {
         statement.executeUpdate(s"DELETE FROM team_in_treasure_hunt WHERE id_treasure_hunt = ${idTH} AND id_team = ${idTeam}")
 
         connection.close()
+    }
+
+    test("If a team name is not available, checkTeamNameAvailability must return false, otherwise true") {
+        val availableName: String = "available name"
+        val notAvailableName: String = "team ammut"
+
+        /*---CHECK IF INSERT IT IS AVAILABLE---*/
+        var availability = teamDB.checkTeamNameAvailability(availableName)
+        assert(availability)
+
+        /*---CHECK IF INSERT IT IS AVAILABLE---*/
+        availability = teamDB.checkTeamNameAvailability(notAvailableName)
+        assert(!availability)
+
+    }
+
+    test("After creating a new team, it is present in team table with the correct information") {
+        val connection: Connection = connectionManager.establishConnection
+        var statement = connection.createStatement
+
+        val teamName: String = "new team"
+        val password: String = "che campioni holly e benji"
+
+        teamDB.createNewTeam(teamName, password);
+
+        /*---CHECK IF INSERT IS CORRECT---*/
+        var rs = statement.executeQuery(s"SELECT COUNT(*) FROM team WHERE name = '${teamName.toLowerCase}' AND password = '${password}'")
+        while (rs.next) {
+            assert(rs.getInt(1) == 1)
+        }
+
+        /*---DELETE THE LAST ADDED ROW---*/
+        statement = connection.createStatement
+        statement.executeUpdate(s"DELETE FROM team WHERE name = '${teamName}'")
+
+        connection.close()
+    }
+
+    test("if the login credentials are correct, login must return true, otherwise false") {
+        val correctTeamName: String = "team ammut"
+        val correctPassword: String = "password"
+
+        val wrongTeamName: String = "wrong team"
+        val wrongPassword: String = "wrong password"
+
+        assert(teamDB.login(correctTeamName, correctPassword))
+        assert(!teamDB.login(correctTeamName, wrongPassword))
+        assert(!teamDB.login(wrongTeamName, correctPassword))
+        assert(!teamDB.login(wrongTeamName, wrongPassword))
     }
 }
