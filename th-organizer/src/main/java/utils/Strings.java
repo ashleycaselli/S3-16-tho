8fbec0a4e44c608764539cc3c5e9b5063c0e1bce
Ashley Caselli <ashley.caselli@studio.unibo.it>
2017-08-29 23:23:02 +0200

    MapView added, Strings updated, THListCellRenderer created

--- th-organizer/src/main/java/utils/Strings.java
+++ th-organizer/src/main/java/utils/Strings.java
@@ -14,4 +14,17 @@ public class Strings {
     public final static String LOAD_BUTTON = "Load";
 
     public final static String DELETE_BUTTON = "Delete";
+
+    public final static String ADD_POI_BUTTON = "Add Point of Interest";
+
+    public final static String SET_QUIZ_BUTTON = "Set Quiz";
+
+    public final static String SET_CLUE_BUTTON = "Set Clue";
+
+    public final static String CREATE_HUNT_CODE_BUTTON = "Create Hunt Code";
+
+    public final static String START_HUNT_BUTTON = "Start Hunt";
+
+    public final static String SHOW_PLAYERS_BUTTON = "Show Players on Map";
+    
 }
---
+++ th-organizer/src/main/java/view/MapView.java
@@ -0,0 +1,79 @@
+package view;
+
+import javafx.application.Platform;
+import utils.Resources;
+import utils.Strings;
+import view.component.GoogleMapsPanel;
+import view.component.LogoPanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapView extends JFrame {
+
+    public static final Dimension FRAME_DIMENSION = new Dimension(1024, 768);
+    public static final int DEFAULT_INSET_VALUE = 10;
+
+    private LogoPanel logoPanel;
+    private final List<JButton> buttonList = new ArrayList();
+    private GoogleMapsPanel googleMapsPanel;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new MapView());
+    }
+
+    public MapView() {
+        init();
+    }
+
+    private void init() {
+        this.getContentPane().setBackground(Color.white);
+        this.setLayout(new GridBagLayout());
+        this.setTitle(Strings.ORGANIZER_TITLE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(FRAME_DIMENSION);
+        this.setResizable(false);
+        this.initComponent();
+        this.setVisible(true);
+
+        Platform.runLater(() -> this.googleMapsPanel.initFX());
+    }
+
+    private void initComponent() {
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(DEFAULT_INSET_VALUE, DEFAULT_INSET_VALUE, DEFAULT_INSET_VALUE, DEFAULT_INSET_VALUE);
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.CENTER;
+
+        this.logoPanel = new LogoPanel(LogoPanel.Position.LEFT);
+        this.add(this.logoPanel, c);
+
+        this.buttonList.add(new JButton(Strings.ADD_POI_BUTTON));
+        this.buttonList.add(new JButton(Strings.SET_QUIZ_BUTTON));
+        this.buttonList.add(new JButton(Strings.SET_CLUE_BUTTON));
+        this.buttonList.add(new JButton(Strings.CREATE_HUNT_CODE_BUTTON));
+        this.buttonList.add(new JButton(Strings.START_HUNT_BUTTON));
+        this.buttonList.add(new JButton(Strings.SHOW_PLAYERS_BUTTON));
+        c.anchor = GridBagConstraints.LINE_START;
+        this.buttonList.forEach(button -> {
+            button.setFont(Resources.DEFAULT_FONT);
+            c.gridy++;
+            this.add(button, c);
+        });
+
+        this.googleMapsPanel = new GoogleMapsPanel();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridy = 0;
+        c.gridheight = 7;
+        this.add(this.googleMapsPanel, c);
+
+    }
+
+
+}
