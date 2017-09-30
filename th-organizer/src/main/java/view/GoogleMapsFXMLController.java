68a68a356580ee6092db44e48692826a51fce52b
Francesca Tassinari <francesc.tassinari10@studio.unibo.it>
2017-09-30 17:03:21 +0200

    Desktop - new Poi creation

--- th-organizer/src/main/java/view/GoogleMapsFXMLController.java
+++ th-organizer/src/main/java/view/GoogleMapsFXMLController.java
@@ -1,30 +1,28 @@
 package view;
 
-import com.lynden.gmapsfx.GoogleMapView;
-import com.lynden.gmapsfx.MapComponentInitializedListener;
-import com.lynden.gmapsfx.javascript.event.UIEventType;
-import com.lynden.gmapsfx.javascript.object.*;
-import com.lynden.gmapsfx.service.geocoding.GeocoderStatus;
-import com.lynden.gmapsfx.service.geocoding.GeocodingResult;
-import com.lynden.gmapsfx.service.geocoding.GeocodingService;
-import controller.THOrganizer$;
-import domain.POIImpl;
-import domain.PositionImpl;
-import domain.TreasureHunt;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputDialog;
-import javafx.stage.StageStyle;
-import utils.Strings;
+        import com.lynden.gmapsfx.GoogleMapView;
+        import com.lynden.gmapsfx.MapComponentInitializedListener;
+        import com.lynden.gmapsfx.javascript.event.UIEventType;
+        import com.lynden.gmapsfx.javascript.object.*;
+        import com.lynden.gmapsfx.service.geocoding.GeocoderStatus;
+        import com.lynden.gmapsfx.service.geocoding.GeocodingResult;
+        import com.lynden.gmapsfx.service.geocoding.GeocodingService;
+        import controller.THOrganizer$;
+        import domain.*;
+        import javafx.beans.property.SimpleStringProperty;
+        import javafx.beans.property.StringProperty;
+        import javafx.fxml.FXML;
+        import javafx.fxml.Initializable;
+        import javafx.scene.control.Alert;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.TextField;
+        import javafx.scene.control.TextInputDialog;
+        import javafx.stage.StageStyle;
+        import utils.Strings;
 
-import java.net.URL;
-import java.util.Optional;
-import java.util.ResourceBundle;
+        import java.net.URL;
+        import java.util.Optional;
+        import java.util.ResourceBundle;
 
 /**
  * Google Maps controller. It allows to render the Google Maps View specified in .fxml file.
@@ -70,24 +68,43 @@ public class GoogleMapsFXMLController implements Initializable, MapComponentInit
         this.map = this.mapView.createMap(mapOptions);
         this.map.addMouseEventHandler(UIEventType.click, mouseEvent -> {
             String name;
+            String clue;
+            String quiz;
+            String answer;
             TextInputDialog dialog = new TextInputDialog("Insert POI name");
             dialog.initStyle(StageStyle.UTILITY);
             Optional<String> result = dialog.showAndWait();
             if (result.isPresent()) {
                 name = result.get();
-                LatLong poiLocation = mouseEvent.getLatLong();
-                MarkerOptions poiMarkerOptions = new MarkerOptions();
-                poiMarkerOptions.position(poiLocation);
-                Marker poiMarker = new Marker(poiMarkerOptions);
-                this.map.addMarker(poiMarker);
-                InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
-                infoWindowOptions.content("<h4>" + name + "</h4>");
-                InfoWindow poiWindow = new InfoWindow(infoWindowOptions);
-                poiWindow.open(this.map, poiMarker);
-                THOrganizer$.MODULE$.instance().addPoi(new POIImpl(new PositionImpl(poiLocation.getLatitude(), poiLocation.getLongitude()), name, this.currentTreasureHunt.ID(), null, null));
+                dialog = new TextInputDialog("Clue");
+                result = dialog.showAndWait();
+                if (result.isPresent()) {
+                    clue = result.get();
+                    dialog = new TextInputDialog("Quiz");
+                    result = dialog.showAndWait();
+                    if (result.isPresent()) {
+                        quiz = result.get();
+                        dialog = new TextInputDialog("Answer");
+                        result = dialog.showAndWait();
+                        if (result.isPresent()) {
+                            answer = result.get();
+                            LatLong poiLocation = mouseEvent.getLatLong();
+                            MarkerOptions poiMarkerOptions = new MarkerOptions();
+                            poiMarkerOptions.position(poiLocation);
+                            Marker poiMarker = new Marker(poiMarkerOptions);
+                            GoogleMapsFXMLController.this.map.addMarker(poiMarker);
+                            InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
+                            infoWindowOptions.content("<h4>" + name + "</h4>");
+                            InfoWindow poiWindow = new InfoWindow(infoWindowOptions);
+                            poiWindow.open(GoogleMapsFXMLController.this.map, poiMarker);
+                            THOrganizer$.MODULE$.instance().addPoi(new POIImpl(new PositionImpl(poiLocation.getLatitude(), poiLocation.getLongitude()), name, GoogleMapsFXMLController.this.currentTreasureHunt.ID(), new QuizImpl(quiz, answer), new ClueImpl(clue)));
+                        }
+                    }
+                }
             }
         });
     }
+
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
